8c8ecfb20507b0ee6318d381e0f3dbb6619ec644
Carles Escrig Royo <esroyo@gmail.com>
2016-03-27 21:37:45 +0200

    Add resize/crop

--- visualdiff.py
+++ visualdiff.py
@@ -36,6 +36,11 @@ settings.configure(
     STATIC_URL='/static/',
 )
 
+import base64
+import urllib
+from io import BytesIO, StringIO
+from PIL import Image, ImageDraw
+from math import floor
 from django import forms
 from django.conf.urls import url
 from django.core.cache import cache
@@ -51,10 +56,75 @@ class ImageForm(forms.Form):
     before = forms.URLField(required=True)
     after = forms.URLField(required=True)
 
+    def generateImages(self):
+        before = urllib.request.urlopen(self.cleaned_data['before'])
+        after = urllib.request.urlopen(self.cleaned_data['after'])
+
+        before = BytesIO(before.read())
+        after = BytesIO(after.read())
+
+        before = Image.open(before)
+        after = Image.open(after)
+
+        # calc maximum common side size to resize
+        before_size = list(before.size)
+        after_size = list(after.size)
+
+        if after.size[0] > before.size[0] and after.size[1] > before.size[1]:
+            w_diff = after.size[0] - before.size[0]
+            h_diff = after.size[1] - before.size[1]
+            if (w_diff < h_diff):
+                after_size[0], after_size[1] = after.size[0] - w_diff, after.size[1] - floor(w_diff / after.size[0] * after.size[1])
+            else:
+                after_size[0], after_size[1] = after.size[0] - floor(h_diff / after_size[1] * after.size[0]), after.size[1] - h_diff
+        elif before.size[0] > after.size[0] and before.size[1] > after.size[1]:
+            w_diff = before.size[0] - after.size[0]
+            h_diff = before.size[1] - after.size[1]
+            if (w_diff < h_diff):
+                before_size[0], before_size[1] = before.size[0] - w_diff, before.size[1] - floor(w_diff / before.size[0] * before.size[1])
+            else:
+                before_size[0], before_size[1] = before.size[0] - floor(h_diff / before_size[1] * before.size[0]), before.size[1] - h_diff
+
+        # calc overflows after resize
+        before_crop = [0, 0, before_size[0], before_size[1]]
+        after_crop = [0, 0, after_size[0], after_size[1]]
+
+        # calc width to crop
+        if after_size[0] > before_size[0]:
+            diff = floor((after_size[0] - before_size[0]) / 2)
+            after_crop[0], after_crop[2] = diff, after_size[0] - diff
+        elif before_size[0] > after_size[0]:
+            diff = floor((before_size[0] - after_size[0]) / 2)
+            before_crop[0], before_crop[2] = diff, before_size[0] - diff
+
+        # calc height to crop
+        if after_size[1] > before_size[1]:
+            diff = floor((after_size[1] - before_size[1]) / 2)
+            after_crop[1], after_crop[3] = diff, after_size[1] - diff
+        elif before_size[1] > after_size[1]:
+            diff = floor((before_size[1] - after_size[1]) / 2)
+            before_crop[1], before_crop[3] = diff, before_temp.size[1] - diff
+
+        # do resize and crop
+        before_resized = before.resize(before_size, Image.BILINEAR)
+        before_cropped = before_resized.crop(before_crop)
+        after_resized = after.resize(after_size, Image.BILINEAR)
+        after_cropped = after_resized.crop(after_crop)
+
+        before_content = BytesIO()
+        before_cropped.save(before_content, before.format)
+        before = 'data:image/{};base64,{}'.format(before.format, base64.b64encode(before_content.getvalue()).decode())
+
+        after_content = BytesIO()
+        after_cropped.save(after_content, after.format)
+        after = 'data:image/{};base64,{}'.format(after.format, base64.b64encode(after_content.getvalue()).decode())
+
+        return before, after
+
 def generate_etag(request):
     before = request.GET.get('before')
     after = request.GET.get('after')
-    content = 'Diff: {0} x {1}'.format(before, after)
+    content = 'Diff: {} x {}'.format(before, after)
     return hashlib.sha1(content.encode('utf-8')).hexdigest()
 
 @etag(generate_etag)
@@ -63,6 +133,7 @@ def index(request):
     after = request.GET.get('after')
     form = ImageForm({'before': before, 'after': after})
     if form.is_valid():
+        before, after = form.generateImages()
         context = {
             'before': before,
             'after': after
